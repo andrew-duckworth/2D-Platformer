97c29d77b1e88d8476fec11e0bbfb56b18ee9b98
student <student@pop-os.localdomain>
2023-04-14 12:21:42 +1200

    new Save

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,29 @@ public class PlayerMovement : MonoBehaviour
 { 
   public Rigidbody2D playerRb;
   public float speed;
+
   //tracks x (left or right)
   public float input;
+
+  //grabs sprite image
   public SpriteRenderer spriteRenderer;
   public float jumpForce;
 
+//ground layer check
   public LayerMask groundLayer;
   private bool isGrounded;
+
+  //feet pos check
   public Transform feetPosition;
   public float groundCheckCircle;
+
+  //checks how long you are in the air, max = jumpTime
+  public float jumpTime = 0.35f;
+  public float jumpTimeCounter;
+
+  //check if jump has already been initiated to prevent double jump
+  private bool isJumping;
+
 
     // Update is called once per frame
     void Update()
@@ -34,11 +48,31 @@ public class PlayerMovement : MonoBehaviour
 
       isGrounded = Physics2D.OverlapCircle(feetPosition.position, groundCheckCircle, groundLayer);
 
-      if(isGrounded == true && Input.GetButton("Jump"))
+      if(isGrounded == true && Input.GetKeyDown(KeyCode.UpArrow))
       {
+        isJumping = true;
+        jumpTimeCounter = jumpTime;
         playerRb.velocity = Vector2.up * jumpForce;
       }
 
+      if (Input.GetKey(KeyCode.UpArrow) && isJumping == true)
+      {
+        if(jumpTimeCounter > 0)
+        {
+          playerRb.velocity = Vector2.up * jumpForce;
+          jumpTimeCounter -= Time.deltaTime;
+        }
+        else
+        {
+          isJumping = false;
+        }
+
+      }
+
+      if(Input.GetKeyUp(KeyCode.UpArrow))
+      {
+        isJumping = false;
+      }
 
     }
 
